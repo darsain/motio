5447696da4b77fc4e6329c9aa487e9ee26a054bd
Tomas Sardyha <darsain@gmail.com>
2013-04-05 18:43:15 +0200

    Restructuring repo

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+/*jshint node:true */
+module.exports = function(grunt) {
+	'use strict';
+
+	// Override environment based line endings enforced by Grunt
+	grunt.util.linefeed = '\n';
+
+	// Grunt configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('component.json'),
+		meta: {
+			banner: '/*!\n' +
+				' * <%= pkg.name %> <%= pkg.version %> - <%= grunt.template.today("dS mmm yyyy") %>\n' +
+				' * <%= pkg.homepage %>\n' +
+				' *\n' +
+				' * Licensed under the <%= pkg.licenses[0].type %> license.\n' +
+				' * <%= pkg.licenses[0].url %>\n' +
+				' */\n',
+			bannerLight: '/*! <%= pkg.name %> <%= pkg.version %>' +
+				' - <%= grunt.template.today("dS mmm yyyy") %> | <%= pkg.homepage %> */'
+		},
+
+		// JSHint the code.
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: ['src/*.js']
+		},
+
+		// Clean folders.
+		clean: {
+			dist: ['dist/**', '!dist']
+		},
+
+		// Concatenate files.
+		concat: {
+			options: {
+				banner: '<%= meta.banner %>'
+			},
+			vanilla: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'dist/<%= pkg.name %>.js'
+			},
+			jquery: {
+				src: ['src/<%= pkg.name %>.js', 'src/jquery.js'],
+				dest: 'dist/jquery.<%= pkg.name %>.js'
+			}
+		},
+
+		// Minify with Google Closure Compiler.
+		gcc: {
+			options: {
+				banner: '<%= meta.bannerLight %>'
+			},
+			vanilla: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'dist/<%= pkg.name %>.min.js'
+			},
+			jquery: {
+				src: ['src/<%= pkg.name %>.js', 'src/jquery.js'],
+				dest: 'dist/jquery.<%= pkg.name %>.min.js'
+			}
+		},
+
+		// Compress files.
+		compress: {
+			options: {
+				mode: 'gzip'
+			},
+			vanilla: {
+				src: 'dist/<%= pkg.name %>.min.js',
+				dest: 'dist/<%= pkg.name %>.min.js.gz'
+			},
+			jquery: {
+				src: 'dist/<%= pkg.name %>.jquery.min.js',
+				dest: 'dist/<%= pkg.name %>.jquery.min.js.gz'
+			}
+		},
+
+		// Bump up fields in JSON files.
+		bumpup: ['component.json', '<%= pkg.name %>.jquery.json'],
+
+		// Commit changes and tag the latest commit with a version from JSON file.
+		tagrelease: ['component.json']
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-tagrelease');
+	grunt.loadNpmTasks('grunt-bumpup');
+	grunt.loadNpmTasks('grunt-gcc');
+
+	// Build task.
+	grunt.registerTask('build', function () {
+		grunt.task.run('jshint');
+		grunt.task.run('clean');
+		grunt.task.run('concat');
+		grunt.task.run('gcc');
+	});
+
+	// Release task.
+	grunt.registerTask('release', function (type) {
+		type = type ? type : 'patch';
+		grunt.task.run('build');
+		grunt.task.run('bumpup:' + type);
+		grunt.task.run('tagrelease');
+	});
+
+	// Default task.
+	grunt.registerTask('default', ['jshint']);
+};
