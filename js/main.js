537d4c33c9349eb508927c41bf12b86bad816e85
Tomas Sardyha <darsain@gmail.com>
2013-04-06 22:34:18 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -28,38 +28,69 @@ jQuery(function ($) {
 
 	// Panning
 	(function () {
-		var $frame = $('.examples .panning');
-		var offset = $frame.offset();
-		var motio = new Motio($frame[0], {
+		var $example = $('#panning .example');
+		var frame = $example.find('.frame')[0];
+		var offset = $example.offset();
+		var motio = new Motio(frame, {
 			fps: 60,
 			bgWidth: 1024,
 			bgHeight: 1024
 		});
 
-		// Play when mouse enters the frame, and pause when it leaves
-		$frame.on('mouseenter mouseleave', function (event) {
-			offset = $frame.offset();
-			motio[event.type === 'mouseenter' ? 'play' : 'pause']();
+		// Play/Pause when mouse enters/leaves the frame
+		$example.on('mouseenter mouseleave', function (event) {
+			if (event.type === 'mouseenter') {
+				motio.play();
+			} else {
+				motio.pause();
+			}
 		});
 
-		// Update animation speed & direction based on a cursor position
-		$frame.on('mousemove', function (event) {
+		// Update example offset offset on window resize
+		$(window).on('resize', function () {
+			offset = $example.offset();
+		});
+
+		// Update the animation speed & direction based on a cursor position
+		$example.on('mousemove', function (event) {
 			motio.set('speedX', event.pageX - offset.left - motio.width / 2);
 			motio.set('speedY', event.pageY - offset.top - motio.height / 2);
 		});
 	}());
 
 	// Sprite
-	(function () {
-		var $frame = $('.examples .sprite');
-		var motio = new Motio($frame[0], {
-			frames: 14,
-			fps: 60
+	$('a[data-toggle="tab"][href=#sprite]').one('shown', function () {
+		var $example = $('#sprite .example');
+		var frame = $example.find('.frame')[0];
+		var motio = new Motio(frame, {
+			fps: 10,
+			frames: 14
 		});
 
 		// Play when mouse enters the frame, and pause when it leaves
-		$frame.on('mouseenter mouseleave', function (event) {
+		$example.on('mouseenter mouseleave', function (event) {
 			motio[event.type === 'mouseenter' ? 'play' : 'pause']();
 		});
-	}());
+	});
+
+	// 360 view
+	$('a[data-toggle="tab"][href=#circview]').one('shown', function () { console.log('asd');
+		var $example = $('#circview .example');
+		var exampleLeft = $example.offset().left;
+		var exampleWidth = $example.width();
+		var frame = $example.find('.frame')[0];
+		var motio = new Motio(frame, {
+			frames: 18
+		});
+
+		// Update example left offset on window resize
+		$(window).on('resize', function () {
+			exampleLeft = $example.offset().left;
+		});
+
+		// Activate frame based on the cursor position
+		$example.on('mousemove', function (event) {
+			motio.to(Math.floor(motio.frames / exampleWidth * (event.pageX - exampleLeft)), true);
+		});
+	});
 });
