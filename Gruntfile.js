87aa3ce4b33b4b17a0361838ea08f3149a4b3300
Tomas Sardyha <darsain@gmail.com>
2013-12-05 00:16:23 +0100

    Updating Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*jshint node:true */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	'use strict';
 
 	// Override environment based line endings enforced by Grunt
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
 
 	// Grunt configuration
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('bower.json'),
+		pkg: grunt.file.readJSON('meta.json'),
 		meta: {
 			banner: '/*!\n' +
 				' * <%= pkg.title %> <%= pkg.version %> - <%= grunt.template.today("dS mmm yyyy") %>\n' +
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
 		// JSHint the code.
 		jshint: {
 			options: {
-				jshintrc: '.jshintrc'
+				jshintrc: true
 			},
 			all: ['src/*.js']
 		},
@@ -63,42 +63,27 @@ module.exports = function(grunt) {
 			}
 		},
 
-		// Compress files.
-		compress: {
+		// Bump up fields in JSON files.
+		bumpup: {
 			options: {
-				mode: 'gzip'
+				updateProps: {
+					pkg: 'meta.json',
+				},
 			},
-			vanilla: {
-				src: 'dist/<%= pkg.name %>.min.js',
-				dest: 'dist/<%= pkg.name %>.min.js.gz'
-			},
-			jquery: {
-				src: 'dist/jquery.<%= pkg.name %>.min.js',
-				dest: 'dist/jquery.<%= pkg.name %>.min.js.gz'
-			}
+			files: ['meta.json', '<%= pkg.name %>.jquery.json'],
 		},
 
-		// Bump up fields in JSON files.
-		bumpup: ['component.json', '<%= pkg.name %>.jquery.json'],
-
 		// Commit changes and tag the latest commit with a version from JSON file.
-		tagrelease: 'component.json'
+		tagrelease: '<%= pkg.version %>'
 	});
 
 	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-compress');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-tagrelease');
 	grunt.loadNpmTasks('grunt-bumpup');
 	grunt.loadNpmTasks('grunt-gcc');
-
-	// Task for updating the pkg config property. Needs to be run after
-	// bumpup so the next tasks in queue can work with updated values.
-	grunt.registerTask('updatePkg', function () {
-		grunt.config.set('pkg', grunt.file.readJSON('component.json'));
-	});
 
 	// Build task.
 	grunt.registerTask('build', function () {
@@ -112,7 +97,6 @@ module.exports = function(grunt) {
 		type = type ? type : 'patch';
 		grunt.task.run('jshint');
 		grunt.task.run('bumpup:' + type);
-		grunt.task.run('updatePkg');
 		grunt.task.run('build');
 		grunt.task.run('tagrelease');
 	});
